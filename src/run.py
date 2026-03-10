f1ff4daf1f2c7482f9a03df619ada43367f9674a
hkereb <115366640+hkereb@users.noreply.github.com>
2026-03-10 14:40:38 +0100

    refactor MySystem base class

--- src/run.py
+++ src/run.py
@@ -3,7 +3,7 @@ from RatingLib import User, Movie
 from tqdm import tqdm
 from RatingSystem import RatingSystem, RatingSystemCompetition
 from SampleSystems import NaiveRating, AverageMovieRating, GlobalAverageMovieRating, Cheater, AverageUserRating
-# from secret_system import MySystem as secret
+from system155877 import MySystem as secret
 
 def main():
     #read the movie indices
@@ -24,7 +24,7 @@ def main():
     #create the competition
     competition = RatingSystemCompetition()
     #register systems
-    # competition.register(secret())
+    competition.register(secret())
     competition.register(NaiveRating())
     competition.register(AverageMovieRating())
     competition.register(GlobalAverageMovieRating())
